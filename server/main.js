f8147fa7adbad6307f8a4ed5d152b70a395acbd5
gmoranfdez <gmoranfdez@gmail.com>
2016-09-29 17:14:28 +0200

    arquitectura de paquetes
    inclusión de un sistema organizado de carpetas. borrar lo que no se use

--- server/main.js
+++ server/main.js
@@ -1,5 +1,23 @@
 import { Meteor } from 'meteor/meteor';
 
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+FlowRouter.route('/', {
+  action: function() {
+    BlazeLayout.render('layout', { main: 'home' });
+  }
+});
+
+FlowRouter.route('/blog', {
+  action: function() {
+    BlazeLayout.render('layout', { main: 'blog' });
+  }
+});
+
+FlowRouter.route('/calendario');
+FlowRouter.route('/estadosgenerales');
+FlowRouter.route('/calendario');
+FlowRouter.route('/contact');
